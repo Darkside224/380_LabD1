99c64a0d376daa40352c9343055ed105adefb3fa
Darkside224 <benzuidema6@gmail.com>
2023-02-09 14:42:01 -0700

    thing

--- edu.ucalgary.oop/ManageAccess.java
+++ edu.ucalgary.oop/ManageAccess.java
@@ -14,7 +14,7 @@ public class ManageAccess {
 	public ManageAccess(String[] logFile) throws IllegalArgumentException {
 		for (int i=0; i < logFile.length; i++) {
 			var logLine = new AccessLog(logFile[i]);
-			completeLog[i] = logLine
+			completeLog[i] = logLine;
 		}
 	}
 
@@ -25,3 +25,4 @@ public class ManageAccess {
 		}
 		throw new IllegalArgumentException("Array only holds 0-9");
 	}
+}
